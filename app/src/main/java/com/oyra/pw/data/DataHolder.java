ce10ffa3632fe023637cd24c52680c880740f380
oyra <aka.oyra@gmail.com>
2015-12-25 03:08:51 +0300

    TODO

--- app/src/main/java/com/oyra/pw/data/DataHolder.java
+++ app/src/main/java/com/oyra/pw/data/DataHolder.java
@@ -9,6 +9,10 @@ public class DataHolder {
 
     public static PriorityBlockingQueue<Unit> sQueue = new PriorityBlockingQueue<>();
 
+    //TODO In real situation we’ll need to clear the queue time to time in some way
+    // (removing the low-priority data, dumping data to db) otherwise it may grow and grow
+    // (the velocity of growing depends on services’ timeouts and the errors frequency)
+
     //some queue-cleaning procedures could be here
 
 
